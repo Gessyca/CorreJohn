90d9529dd78233419fe848876c548bb6fe550d14
gessyca <gessyca.lm@gmail.com>
2015-05-11 20:07:50 -0300

    gerenciando geraçao de alimentos

--- game.lua
+++ game.lua
@@ -34,37 +34,41 @@ local pontuacao = 0
 cereja = display.newImage("comidas/s.png")
 cereja:scale(.05,.05)
 cereja.alpha = 0
+cereja.name = "saudavel"
 physics.addBody(cereja, "kinematic")
 coco = display.newImage("comidas/s2.png")
 coco:scale(.08,.08)
 coco.alpha = 0
+coco.name = "saudavel"
 physics.addBody(coco, "kinematic")
 salada = display.newImage("comidas/s3.png")
+salada.name = "saudavel"
 salada:scale(.05,.05)
 salada.alpha = 0
 physics.addBody(salada, "kinematic")
-maca = display.newImage("comidas/s4.png")
-maca:scale(.01,.01)
-maca.alpha = 0
-physics.addBody(maca, "kinematic")
 abacaxi = display.newImage("comidas/s5.png")
+abacaxi.name = "saudavel"
 abacaxi:scale(.01,.01)
 abacaxi.alpha = 0
 physics.addBody(abacaxi, "kinematic")
 
 cerveja = display.newImage("comidas/m.png")
+cerveja.name = "gorduroso"
 cerveja:scale(.15,.15)
 cerveja.alpha = 0
 physics.addBody(cerveja, "kinematic")
 hotDog = display.newImage("comidas/m2.png")
+hotDog.name = "gorduroso"
 hotDog:scale(.05,.05)
 hotDog.alpha = 0
 physics.addBody(hotDog, "kinematic")
 sorvete = display.newImage("comidas/m3.png")
-sorvete:scale(.05,.05)
+sorvete.name = "gorduroso"
+sorvete:scale(.1,.1)
 sorvete.alpha = 0
 physics.addBody(sorvete, "kinematic")
 bolo = display.newImage("comidas/m5.png")
+bolo.name = "gorduroso"
 bolo:scale(.02,.02)
 bolo.alpha = 0
 physics.addBody(bolo, "kinematic")
@@ -90,6 +94,8 @@ local player = display.newSprite(imagem, comandos)
 -- cria finalmente a sprite utilizando as propriedades vistas acima
 player.x = meiox-190
 player.y = meioy+93
+player.name = "John"
+physics.addBody(player, "static")
 player:setSequence("direita")
 
 local teto = display.newRect( _W2, -1, _W+100, 1 )
@@ -102,8 +108,17 @@ piso:setFillColor( 0, 0, 0 )
 physics.addBody( piso, "static" )
 piso.name = "piso"
 
-local alimentoSaudavel
-local alimentoGorduroso
+listaAlimentos = {
+[0] = cerveja,
+[1] = cereja,
+[2] = hotDog,
+[3] = coco,
+[4] = sorvete,
+[5] = salada,
+[6] = bolo,
+[7] = abacaxi
+}
+local alimento
 local tm
 function scene:create( event )
     local sceneGroup = self.view
@@ -122,7 +137,6 @@ function scene:create( event )
 	alimentos:insert( cereja )
 	alimentos:insert( salada )
 	alimentos:insert( abacaxi )
-	alimentos:insert( maca )
 	alimentos:insert( cerveja )
 	alimentos:insert( hotDog )
 	alimentos:insert( sorvete )
@@ -139,9 +153,8 @@ function scene:show( event )
 	aparecerDonuts()
 	Runtime:addEventListener("enterFrame", backgroundLoop)
 	donuts:addEventListener("tap",pular)
-	tm = timer.performWithDelay( 4000,mostrarAlimentosSaudaveis ,0)
-	tm = timer.performWithDelay( 5000,mostrarAlimentosGordurosos ,0)
-	--Runtime:addEventListener("collision", onCollision)
+	tm = timer.performWithDelay( 4000,mostrarAlimentos ,0)
+	Runtime:addEventListener("collision", colisao)
     end
 end
 
@@ -153,7 +166,7 @@ function scene:hide( event )
    if ( phase == "will" ) then
     timer.cancel(tm)
 	donuts:removeEventListener("tap",pular)
-	--Runtime:removeEventListener("collision", onCollision)
+	Runtime:removeEventListener("collision", colisao)
     end
 end
 
@@ -161,59 +174,32 @@ function scene:destroy( event )
     local sceneGroup = self.view
 end
 
-function mostrarAlimentosGordurosos(event)
-
-	alimentoGorduroso = selecionarAlimentoGorduroso()
-	alimentoGorduroso.alpha = 1
-	alimentoGorduroso.x = _W
-	alimentoGorduroso.y = math.random(100, _H )
-	alimentoGorduroso.name = "alimentoGorduroso"
-	alimentoGorduroso.isSensor = true  
-  transition.to( alimentoGorduroso, {time = 3000, x = -50, y = alimentoGorduroso.y, onComplete = removerAlimentoGorduroso})
-
+function mostrarAlimentos(event)
+	indice = math.random(0,7)
+	alimento = listaAlimentos[indice]
+	alimento.alpha = 1
+	alimento.x = _W
+	alimento.y = math.random(meioy, meioy+95 )
+	alimento.isSensor = true  
+  transition.to( alimento, {time = 3000, x = -50, y = alimento.y, onComplete = removerAlimento})
 end
 
-function mostrarAlimentosSaudaveis(event)
-
-	alimentoSaudavel = selecionarAlimentoSaudavel()
-	alimentoSaudavel.alpha = 1
-	alimentoSaudavel.x = _W
-	alimentoSaudavel.y = math.random(100, _H )
-	alimentoSaudavel.name = "alimentoSaudavel"
-	alimentoSaudavel.isSensor = true  
-  transition.to( alimentoSaudavel, {time = 3000, x = -50, y = alimentoSaudavel.y, onComplete = removerAlimentoSaudavel})
-
+local function colisao(event)
+print "entrou no metodo"
+if ( event.phase == "began" ) then
+if(event.object1.name == "saudavel" and event.object2.name == "John") then
+pontuacao = pontuacao + 1
+event.object1.alpha = 0
+end
+if(event.object1.name == "John" and event.object2.name == "saudavel") then
+pontuacao = pontuacao + 1
+event.object2.alpha = 0
+end
+end
 end
 
-function selecionarAlimentoSaudavel()
-local alimentosSaudaveis = {
-[0] = cereja,
-[1] = coco,
-[2] = salada,
-[3] = maca,
-[4] = abacaxi
-}
-return alimentosSaudaveis[math.random (0, 4)]
-end
-
-function selecionarAlimentoGorduroso()
-local alimentosGordurosos = {
-[0] = cerveja,
-[1] = hotDog,
-[2] = sorvete,
-[3] = bolo,
-}
-return alimentosGordurosos[math.random (0, 3)]
-end
-
-function removerAlimentoSaudavel()
-alimentoSaudavel.alpha = 0
---alimentoSaudavel:removeSelf()
-end
-
-function removerAlimentoGorduroso()
-alimentoGorduroso.alpha = 0
---alimentoSaudavel:removeSelf()
+function removerAlimento()
+alimento.alpha = 0
 end
 
 function aparecerDonuts()
@@ -235,7 +221,6 @@ end
 function removerObjeto(event)
 	local obj = event.target
 	display.remove( obj ) 
---  o retorno garante que seja removido somente o objeto clicado
 	return true
 end
 
